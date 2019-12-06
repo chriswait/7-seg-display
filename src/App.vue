46bf81bf3cfc0b3da1969e7d00872b744acb5952
chriswait <chriswait91@gmail.com>
2019-12-06 19:25:02 +0000

    more work on design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div id='scene'>
+      <div id='bg'></div>
       <div id='clock'>
         <div class='face' id='front'><Display :date="date"></Display></div>
-        <div class='face' id='back'></div>
+        <div class='face' id='back'><Display :date="date"></Display></div>
         <div class='face' id='right'></div>
         <div class='face' id='left'></div>
         <div class='face' id='top'></div>
@@ -34,40 +35,74 @@ export default class App extends Vue {
 
 <style>
 html, body {
-  margin: 0px;
+  margin: 0;
+  padding: 0;
+  max-height: 100%;
+  height: 100%;
+  overflow: hidden;
   background-color: black;
 }
 #app {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  --clock-width: 500px;
-  --clock-height: 100px;
-  --clock-depth: 300px;
-}
-#scene {
-  width: var(--clock-width);
-  height: var(--clock-height);
-  perspective: 2000px;
-}
-#clock {
   width: 100%;
   height: 100%;
+}
+
+#app {
+  --clock-width: 250px;
+  --clock-height: 50px;
+  --clock-depth: 150px;
+}
+@media (min-width: 480px) and (max-width: 768px) {
+  #app {
+    --clock-width: 400px;
+    --clock-height: 80px;
+    --clock-depth: 240px;
+  }
+}
+@media (min-width: 768px) and (max-width: 1024px) {
+  #app {
+    --clock-width: 450px;
+    --clock-height: 90px;
+    --clock-depth: 280px;
+  }
+}
+@media (min-width: 1024px) {
+  #app {
+    --clock-width: 550px;
+    --clock-height: 110px;
+    --clock-depth: 330px;
+  }
+}
+
+#scene {
+  width: 100%;
+  height: 100%;
+  perspective: calc(var(--clock-width) * 3);
+  position: relative;
+}
+#bg {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  background: radial-gradient(#232323, black);
+}
+#clock {
+  height: var(--clock-height);
+  width: var(--clock-width);
+  top: calc(50% - var(--clock-height) / 2);
+  left: calc(50% - var(--clock-width) / 2);
   position: relative;
   transform-style: preserve-3d;
-  transform: rotateX(-8deg);
   display: flex;
   align-items: center;
   justify-content: center;
-  animation: rotation 20s infinite linear;
+  animation: rotation 10s infinite ease-in-out;
 }
 .face {
   position: absolute;
-  background-color: #272f2f;
-  border: 1px solid black;
-  border-radius: 5px;
-  transform-style: preserve-3d;
+  background-color: rgba(39, 47, 47, 0.9);
+  border: 3px solid red;
+  border-radius: 3px;
 }
 #front, #back {
   width: var(--clock-width);
@@ -81,19 +116,23 @@ html, body {
   width: var(--clock-width);
   height: var(--clock-depth);
 }
-#front {  transform: rotateX(0deg)    rotateY(0deg)     translateZ(calc(var(--clock-depth) / 2)); }
-#back {   transform: rotateX(0deg)    rotateY(180deg)   translateZ(calc(var(--clock-depth) / 2)); }
-#right {  transform: rotateX(0deg)    rotateY(90deg)    translateZ(calc(var(--clock-width) / 2)); }
-#left {   transform: rotateX(0deg)    rotateY(-90deg)   translateZ(calc(var(--clock-width) / 2)); }
-#top {    transform: rotateX(90deg)   rotateY(0deg)     translateZ(calc(var(--clock-height) / 2)); }
-#bottom { transform: rotateX(-90deg)  rotateY(0deg)     translateZ(calc(var(--clock-height) / 2)); }
+#front {  transform: rotateY(0deg)    translateZ(calc(var(--clock-depth) / 2)); }
+#back {   transform: rotateY(180deg)  translateZ(calc(var(--clock-depth) / 2)); }
+#right {  transform: rotateY(90deg)   translateZ(calc(var(--clock-width) / 2)); }
+#left {   transform: rotateY(-90deg)  translateZ(calc(var(--clock-width) / 2)); }
+#top {    transform: rotateX(90deg)   translateZ(calc(var(--clock-height) / 2)); }
+#bottom { transform: rotateX(-90deg)  translateZ(calc(var(--clock-height) / 2)); }
 
-@-webkit-keyframes rotation {
+#back > * {
+  transform: rotateX(180deg) rotateY(180deg);
+}
+
+@keyframes rotation {
   from {
-    transform: rotateX(-8deg) rotateY(0deg);
+    transform: rotateY(0deg) rotateZ(0deg);
   }
   to {
-    transform: rotateX(-8deg) rotateY(359deg);
+    transform: rotateY(180deg) rotateZ(180deg);
   }
 }
 </style>
